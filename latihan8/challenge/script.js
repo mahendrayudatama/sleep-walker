280b3335842c0cdd7273e2166fb3e5c49c8e3ddc
Mahendra Yuda Tama <mt14785X@cimbniaga.co.id>
2024-10-31 16:03:22 +0700

    ganti pakek epen buat submit tralala

--- latihan8/challenge/script.js
+++ latihan8/challenge/script.js
@@ -3,48 +3,42 @@ const editForm = document.querySelector("#editForm");
 const submitButton = document.querySelector("#submitButton");
 
 let palue = [
-    "name",
-    "role",
-    "availability",
-    "usia",
-    "lokasi",
-    "pengalaman",
-    "email"
-]
+  "name",
+  "role",
+  "availability",
+  "usia",
+  "lokasi",
+  "pengalaman",
+  "email",
+];
 
 let palueIngput = [
-    "formNama",
-    "formRole",
-    "formAvailability",
-    "formUsia",
-    "formLokasi",
-    "formPengalaman",
-    "formEmail"
-]
+  "formNama",
+  "formRole",
+  "formAvailability",
+  "formUsia",
+  "formLokasi",
+  "formPengalaman",
+  "formEmail",
+];
 
 editButton.addEventListener("click", () => {
-    if (editForm.style.display === "flex") {
-        editForm.style.display = "none";
-    }else {
-        editForm.style.display = "flex";
-    }
-    palue.forEach((item, index) => {
-        document.querySelector(`#${palueIngput[index]}`).value =
-        document.querySelector(`#${item}`).innerHTML
-    });
-});
-
-
-
-
-submitButton.addEventListener("click", () => {
-    palueIngput.forEach((item, index) => {
-        document.querySelector(`#${palue[index]}`).innerHTML =
-        document.querySelector(`#${item}`).value
-    });
+  if (editForm.style.display === "flex") {
+    editForm.style.display = "none";
+  } else {
     editForm.style.display = "flex";
+  }
+  palue.forEach((item, index) => {
+    document.querySelector(`#${palueIngput[index]}`).value =
+      document.querySelector(`#${item}`).innerHTML;
+  });
 });
 
-
-
-
+editForm.addEventListener("submit", function(event) {
+  event.preventDefault();
+  palueIngput.forEach((item, index) => {
+    document.querySelector(`#${palue[index]}`).innerHTML =
+      document.querySelector(`#${item}`).value;
+  });
+  editForm.style.display = "none";
+});
